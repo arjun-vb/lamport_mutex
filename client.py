4dd210a302346caf3602a982314d30548e2b26ae
arjun-vb <vbarjun1009@gmail.com>
2022-01-20 15:07:21 -0800

    updated UI

--- client.py
+++ client.py
@@ -1,42 +1,22 @@
-import socket, pickle
-from common import *
+import sys
+import socket
+import pickle
 import heapq
 import hashlib
 import time
 
 from threading import Thread
-
-ip = '127.0.0.1'
-p1 = 7001
-p2 = 7002
-p3 = 7003
-
-cport = input('Port: ')
-pid = cport
-if cport == 7001:
-	pid = 1
-elif cport == 7002:
-	pid = 2
-elif cport == 7003:
-	pid = 3
-else:
-	print("Enter valid process id")
-	exit()
+from common import *
 
 
-myClock = LamportClock(0,pid)
-
+myClock = LamportClock(0,0)
+pid = 0
 requestPriorityQueue = [] 
 replyCount = 0
 c2c_connections = {}
 transactionFlag = False
-
-ClientSocket = socket.socket()
-ClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-ClientSocket.bind((ip, cport))
-host = '127.0.0.1'
-port = 7000
 user_input = ""
+buff_size = 1024
 
 class Connections(Thread):
 	def __init__(self,connection):
@@ -47,35 +27,32 @@ class Connections(Thread):
 		global replyCount
 		global requestPriorityQueue
 		global transactionFlag
+		global myClock
+
 		while True:
 			
-			response = self.connection.recv(1024)
+			response = self.connection.recv(buff_size)
 
 			data = pickle.loads(response)
 
 			myClock.updateClock(data.clock)
-			##print("Current clock of process " + str(pid) + " is " + str(myClock))
+			#print("Current clock of process " + str(pid) + " is " + str(myClock))
 
 			if data.reqType == "MUTEX":
 				requestPriorityQueue.append(LamportClock(data.clock.clock, data.clock.pid))
 				heapq.heapify(requestPriorityQueue)
-				print("Request recieved from " + str(data.fromPid))
+				print("REQUEST recieved from " + str(data.fromPid) + " at " + str(myClock))
 				myClock.incrementClock()
-				print("Reply sent to " + str(data.fromPid))
-				##print("Current clock of process " + str(pid) + " is " + str(myClock))
+				print("REPLY sent to " + str(data.fromPid) + " at " + str(myClock))
+				#print("Current clock of process " + str(pid) + " is " + str(myClock))
 				reply = RequestMessage(pid, myClock, "REPLY")
 
 				self.connection.send(pickle.dumps(reply))
 			
 			if data.reqType == "REPLY":
-				#print ("Data clock at REPLY is " + str(data.clock.clock))
-				#print ("\n Data PID at REPLY is " + str(data.clock.pid))
-				#print(str(data.clock))
-				#for i in requestPriorityQueue:
-				#	print("queue: " + str(i))
 				#if pid == 2:
 				#	time.sleep(5)
-				print("Reply recieved from " + str(data.fromPid))
+				print("REPLY recieved from " + str(data.fromPid) + " at " + str(myClock))
 				replyCount += 1
 				if replyCount == 2 and requestPriorityQueue[0].pid == pid:
 					#print("Execute Transaction")
@@ -85,15 +62,16 @@ class Connections(Thread):
 					heapq.heappop(requestPriorityQueue)
 					heapq.heapify(requestPriorityQueue)
 					replyCount = 0
-					release = RequestMessage(pid, myClock, "RELEASE")
-					myClock.incrementClock()
-					##print("Current clock of process " + str(pid) + " is " + str(myClock))
-					broadcast(pickle.dumps(release))
+					#release = RequestMessage(pid, myClock, "RELEASE")
+					#myClock.incrementClock()
+					#print("Current clock of process " + str(pid) + " is " + str(myClock))
+					#print("Sending RELEASE to all clients")
+					broadcast("RELEASE")
 					transactionFlag = True
 
 			if data.reqType == "RELEASE":
 				#print("Inside release")
-				print("Release recieved from " + str(data.fromPid))
+				print("RELEASE recieved from " + str(data.fromPid) + " at " + str(myClock))
 				heapq.heappop(requestPriorityQueue)
 				heapq.heapify(requestPriorityQueue)
 				if len(requestPriorityQueue) > 0 and requestPriorityQueue[0].pid == pid and replyCount == 2:
@@ -102,208 +80,102 @@ class Connections(Thread):
 					heapq.heappop(requestPriorityQueue)
 					heapq.heapify(requestPriorityQueue)
 					replyCount = 0
-					release = RequestMessage(pid, myClock, "RELEASE")
-					myClock.incrementClock()
-					##print("Current clock of process " + str(pid) + " is " + str(myClock))
-					broadcast(pickle.dumps(release))
+					#release = RequestMessage(pid, myClock, "RELEASE")
+					#myClock.incrementClock()
+					#print("Current clock of process " + str(pid) + " is " + str(myClock))
+					#print("Sending RELEASE to all clients")
+					broadcast("RELEASE")
 					transactionFlag = True
 
 	def handle_transaction(self, data):
 		if user_input == "BAL":
 			request = RequestMessage(pid, myClock,"BALANCE")
 			myClock.incrementClock()
-			print("Balance request sent to server")
-			##print("Current clock of process " + str(pid) + " is " + str(myClock))
+			print("Balance request sent to server" + " at " + str(myClock))
+			#print("Current clock of process " + str(pid) + " is " + str(myClock))
 			c2c_connections[0].sendall(pickle.dumps(request))
 			#print("balance at 91")
 			
-			balance = c2c_connections[0].recv(1024)
+			balance = c2c_connections[0].recv(buff_size)
 			myClock.incrementClock()
-			##print("Current clock of process " + str(pid) + " is " + str(myClock))
+			#print("Current clock of process " + str(pid) + " is " + str(myClock))
+			print("=============================")
 			print("Balance is " + str(balance))
-			transactionFlag = True
+			print("=============================")
+			#transactionFlag = True
 		else:
 			reciever, amount = user_input.split()
 			
 			transaction = Transaction(int(pid), int(reciever), int(amount))
 
 			myClock.incrementClock()
-			##print("Current clock of process " + str(pid) + " is " + str(myClock))
-			print("Balance request sent to server")
+			#print("Current clock of process " + str(pid) + " is " + str(myClock))
+			print("Balance request sent to server" + " at " + str(myClock))
 			request = RequestMessage(pid,myClock,"BALANCE")
 			c2c_connections[0].sendall(pickle.dumps(request))
-			balance = c2c_connections[0].recv(1024)
+			balance = c2c_connections[0].recv(buff_size)
 			myClock.incrementClock()
-			##print("Current clock of process " + str(pid) + " is " + str(myClock))
+			#print("Current clock of process " + str(pid) + " is " + str(myClock))
+			print("======================================")
 			print("Balance before transaction " + str(balance))
 
 			if int(balance) >= int(amount):
 				
 				myClock.incrementClock()
-				##print("Current clock of process " + str(pid) + " is " + str(myClock))
+				#print("Current clock of process " + str(pid) + " is " + str(myClock))
 				request = RequestMessage(pid,myClock,"LAST_BLOCK")
 				c2c_connections[0].send(pickle.dumps(request))
-				last_blck_str = c2c_connections[0].recv(1024)
+				last_blck_str = c2c_connections[0].recv(buff_size)
 				
 				myClock.incrementClock()
-				##print("Current clock of process " + str(pid) + " is " + str(myClock))
+				#print("Current clock of process " + str(pid) + " is " + str(myClock))
 				last_blck = pickle.loads(last_blck_str)
 				block = Block(hashlib.sha256(str(last_blck)).digest(), 
 					Transaction(transaction.sender,transaction.reciever,transaction.amount))
 				request = RequestMessage(pid,data.clock,"ADD_BLOCK", block)
 
 				myClock.incrementClock()
-				##print("Current clock of process " + str(pid) + " is " + str(myClock))
+				#print("Current clock of process " + str(pid) + " is " + str(myClock))
 				c2c_connections[0].send(pickle.dumps(request))
 
-				message = c2c_connections[0].recv(1024)
+				message = c2c_connections[0].recv(buff_size)
 				myClock.incrementClock()
 				#print("Current clock of process " + str(pid) + " is " + str(myClock))
 				print("Transaction was " + str(message))
 				print("Balance after transaction " + str(int(balance) - int(amount)))
+				print("======================================")
 			else:
-				print("Insufficient Balance, cannot proceed with the transaction")
-			transactionFlag = True
+				print("Insufficient Balance")
+				print("======================================")
+			#transactionFlag = True
 						
-try:
-	ClientSocket.connect((host, port))
-	print("Connected to server")
-except socket.error as e:
-	print(str(e))
 
-#new_connection = Connections(ClientSocket)
-#new_connection.start()
-c2c_connections[0] = ClientSocket
-
-def getMutex(clock):
-	msg = RequestMessage(pid, clock, "MUTEX")
-	data_string = pickle.dumps(msg)
-	#time.sleep(5)
-	broadcast(data_string)
-	print("Sending MUTEX request to other clients")
-	#ClientSocket.sendall(data_string)
-
-def broadcast(data):
-	if pid == 1:
-		c2c_connections[2].sendall(data)
-		c2c_connections[3].sendall(data)
-	elif pid == 2:
-		c2c_connections[1].sendall(data)
-		c2c_connections[3].sendall(data)
-	elif pid == 3:
-		c2c_connections[1].sendall(data)
-		c2c_connections[2].sendall(data)
-
-if cport == 7001: 
-	client2client = socket.socket()
-	client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	client2client.bind((ip, 7010))
-	client2client.listen(2)
-
-	conn, client_address = client2client.accept()
-	print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
-	c2c_connections[2] = conn
-	new_client = Connections(conn)
-	new_client.start()
-
-	conn, client_address = client2client.accept()
-	print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
-	c2c_connections[3] = conn
-	new_client1 = Connections(conn)
-	new_client1.start()
-
-if cport == 7002:
-	client2client = socket.socket()
-	client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	client2client.bind((ip, 7020))
-	
-	try:
-		client2client.connect((host, 7010))
-		print('Connected to: ' + host + ':' + str(7010))
-	except socket.error as e:
-		print(str(e))
-
-	c2c_connections[1] = client2client
-	new_connection = Connections(client2client)
-	new_connection.start()
-
-	client2client = socket.socket()
-	client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	client2client.bind((ip, 7021))
-	client2client.listen(2)
-
-	conn, client_address = client2client.accept()
-	print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
-	c2c_connections[3] = conn
-	new_client1 = Connections(conn)
-	new_client1.start()
-
-if cport == 7003:
-	client2client = socket.socket()
-	client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	client2client.bind((ip, 7030))
-	
-	try:
-		client2client.connect((host, 7010))
-		print('Connected to: ' + host + ':' + str(7010))
-	except socket.error as e:
-		print(str(e))
-
-	c2c_connections[1] = client2client
-	new_connection = Connections(client2client)
-	new_connection.start()
-
-	client2client = socket.socket()
-	client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	client2client.bind((ip, 7031))
-	
-	try:
-		client2client.connect((host, 7021))
-		print('Connected to: ' + host + ':' + str(7021))
-	except socket.error as e:
-		print(str(e))
-
-	c2c_connections[2] = client2client
-	new_connection1 = Connections(client2client)
-	new_connection1.start()
-
-print("Current Balance is $10")
-
-while True:
-	transactionFlag = False
-	print("=======================================")
-	print("*For Balance - BAL")
-	print("*For transferring money - RECV_ID AMOUNT Eg.(2 5)")
-	print("*For quit - Q")
-	print("=======================================")
-	user_input = raw_input()
-
-	if user_input != "Q" and user_input != "BAL" and len(user_input.split()) != 2:
-		print("Please enter valid input")
-		continue
-
-	if user_input == "Q":
-		break
-	
+def sendRequest(client, reqType):
 	myClock.incrementClock()
 	#print("Current clock of process " + str(pid) + " is " + str(myClock))
-	requestPriorityQueue.append(LamportClock(myClock.clock, myClock.pid))
-	heapq.heapify(requestPriorityQueue)
-	#print ("Data clock at creation is " + str(myClock.clock))
-	#print ("\n Data PID at creation is " + str(myClock.pid))
-	#print(str(myClock))
-	replyCount = 0
-	
-	getMutex(myClock)
+	if reqType == "MUTEX":
+		print("REQUEST sent to " + str(client) + " at " + str(myClock))
+	elif reqType == "RELEASE":
+		print("RELEASE sent to " + str(client) + " at " + str(myClock))
 
-	while transactionFlag == False:
-		a=1
-		# transaction response
+	msg = RequestMessage(pid, myClock, reqType)
+	data_string = pickle.dumps(msg)
+	c2c_connections[client].sendall(data_string)
 
 
+def broadcast(reqType):
+	if pid == 1:
+		sendRequest(2,reqType)
+		sendRequest(3,reqType)		
+	elif pid == 2:		
+		sendRequest(1,reqType)
+		sendRequest(3,reqType)
+	elif pid == 3:		
+		sendRequest(1,reqType)
+		sendRequest(2,reqType)
 
 def closeSockets():
-	ClientSocket.close()
+	c2c_connections[0].close()
 	if pid == 1:
 		c2c_connections[2].close()
 		c2c_connections[3].close()
@@ -314,4 +186,149 @@ def closeSockets():
 		c2c_connections[1].close()
 		c2c_connections[2].close()
 
-closeSockets()
+
+def main():
+	ip = '127.0.0.1'
+	server_port = 7000
+	client_port = 0
+	global pid
+	global user_input
+	global myClock
+	global replyCount
+	global transactionFlag
+
+	if sys.argv[1] == "p1":
+		client_port = 7001
+		pid = 1
+	elif sys.argv[1] == "p2":
+		client_port = 7002
+		pid = 2
+	elif sys.argv[1] == "p3":
+		client_port = 7003
+		pid = 3
+	else:
+		print("Enter valid process id")
+		exit()
+
+	ClientSocket = socket.socket()
+	ClientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	ClientSocket.bind((ip, client_port))
+
+	myClock = LamportClock(0,pid)
+	try:
+		ClientSocket.connect((ip, server_port))
+		print("Connected to server")
+	except socket.error as e:
+		print(str(e))
+
+	c2c_connections[0] = ClientSocket
+
+	if client_port == 7001: 
+		client2client = socket.socket()
+		client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		client2client.bind((ip, 7010))
+		client2client.listen(2)
+
+		conn, client_address = client2client.accept()
+		print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
+		c2c_connections[2] = conn
+		new_client = Connections(conn)
+		new_client.start()
+
+		conn, client_address = client2client.accept()
+		print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
+		c2c_connections[3] = conn
+		new_client = Connections(conn)
+		new_client.start()
+
+	if client_port == 7002:
+		client2client = socket.socket()
+		client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		client2client.bind((ip, 7020))
+		
+		try:
+			client2client.connect((ip, 7010))
+			print('Connected to: ' + ip + ':' + str(7010))
+		except socket.error as e:
+			print(str(e))
+
+		c2c_connections[1] = client2client
+		new_connection = Connections(client2client)
+		new_connection.start()
+
+		client2client = socket.socket()
+		client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		client2client.bind((ip, 7021))
+		client2client.listen(2)
+
+		conn, client_address = client2client.accept()
+		print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
+		c2c_connections[3] = conn
+		new_client1 = Connections(conn)
+		new_client1.start()
+
+	if client_port == 7003:
+		client2client = socket.socket()
+		client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		client2client.bind((ip, 7030))
+		
+		try:
+			client2client.connect((ip, 7010))
+			print('Connected to: ' + ip + ':' + str(7010))
+		except socket.error as e:
+			print(str(e))
+
+		c2c_connections[1] = client2client
+		new_connection = Connections(client2client)
+		new_connection.start()
+
+		client2client = socket.socket()
+		client2client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		client2client.bind((ip, 7031))
+		
+		try:
+			client2client.connect((ip, 7021))
+			print('Connected to: ' + ip + ':' + str(7021))
+		except socket.error as e:
+			print(str(e))
+
+		c2c_connections[2] = client2client
+		new_connection1 = Connections(client2client)
+		new_connection1.start()
+
+	print("=======================================================")
+	print("Current Balance is $10")
+
+	while True:
+		transactionFlag = False
+		print("=======================================================")
+		print("| For Balance type 'BAL'                              |")
+		print("| For transferring money - RECV_ID AMOUNT Eg.(2 5)    |")
+		print("| To quit type 'Q'                                    |") 
+		print("=======================================================")
+		user_input = raw_input()
+
+		if user_input != "Q" and user_input != "BAL" and len(user_input.split()) != 2:
+			print("Please enter valid input")
+			continue
+
+		if user_input == "Q":
+			break
+		
+		myClock.incrementClock()
+		#print("Current clock of process " + str(pid) + " is " + str(myClock))
+		requestPriorityQueue.append(LamportClock(myClock.clock, myClock.pid))
+		heapq.heapify(requestPriorityQueue)
+		replyCount = 0
+		
+		broadcast("MUTEX")
+
+		while transactionFlag == False:
+			a=1
+
+
+	closeSockets()
+
+if __name__ == "__main__":
+    main()
+
--- server.py
+++ server.py
@@ -1,43 +1,16 @@
-import socket, pickle
+import socket
+import pickle
 import os
-#from thread import *
-from threading import Thread
-from common import *
 import hashlib
 import time
 
+from threading import Thread
+from common import *
+
+
 client_list = {}
-
-ServerSocket = socket.socket()
-host = '127.0.0.1'
-port = 7000
-
-
-try:	
-	ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	ServerSocket.bind((host, port))
-except socket.error as e:
-	print(str(e))
-
-print('Waiting for a Connection..')
-ServerSocket.listen(5)
-
-
 Blockchain = []
-
-def initializeBlockchain():
-	block = Block(0,Transaction(0, 1, 10))
-	Blockchain.append(block)
-
-	block = Block(hashlib.sha256(str(block)).digest(), Transaction(0,2,10))
-	Blockchain.append(block)
-
-	block = Block(hashlib.sha256(str(block)).digest(), Transaction(0,3,10))
-	Blockchain.append(block)
-
-
-initializeBlockchain()
-
+buff_size = 1024
 
 class Server_Thread(Thread):
 	def __init__(self,connection,name,port):
@@ -52,10 +25,9 @@ class Server_Thread(Thread):
 
 	def handle_messages(self):
 		while True:
-			request = self.connection.recv(1024)
+			request = self.connection.recv(buff_size)
 			data = pickle.loads(request)
-			#print(data.reqType)
-			#print(data.fromPid)
+
 			if data.reqType == "BALANCE":
 				self.handle_balance(data)
 			elif data.reqType == "LAST_BLOCK":
@@ -64,7 +36,7 @@ class Server_Thread(Thread):
 				self.add_block(data)
 			
 	def handle_balance(self, data):
-		time.sleep(5)
+		#time.sleep(5)
 		balance = 0
 		for blk in Blockchain:
 			if blk.transaction.sender == data.fromPid:
@@ -82,38 +54,69 @@ class Server_Thread(Thread):
 		client_list[data.fromPid].connection.sendall("SUCCESSFUL")
 
 
-connection, client_address = ServerSocket.accept()
-print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
-new_client= Server_Thread(connection, client_address[0] , client_address[1])
-new_client.start()
-client_list[client_address[1]%7000] = new_client
 
-connection, client_address = ServerSocket.accept()
-print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
-new_client= Server_Thread(connection, client_address[0] , client_address[1])
-new_client.start()
-client_list[client_address[1]%7000] = new_client
+def initializeBlockchain():
+	block = Block(0,Transaction(0, 1, 10))
+	Blockchain.append(block)
 
-connection, client_address = ServerSocket.accept()
-print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
-new_client= Server_Thread(connection, client_address[0] , client_address[1])
-new_client.start()
-client_list[client_address[1]%7000] = new_client
+	block = Block(hashlib.sha256(str(block)).digest(), Transaction(0,2,10))
+	Blockchain.append(block)
 
+	block = Block(hashlib.sha256(str(block)).digest(), Transaction(0,3,10))
+	Blockchain.append(block)
 
 def print_blockchain():
-	print("=======================================")
+	print("=========================================")
 	for blk in Blockchain:
 		print("Sender: " + str(blk.transaction.sender) + " | Reciever: " + str(blk.transaction.reciever)
 			+ " | Amount: " + str(blk.transaction.amount)) 
-	print("=======================================")
-while True:
-	print("=======================================")
-
-	user_input = raw_input()
-	if user_input == "PRINT":
-		print_blockchain()
+	print("=========================================")
 
 
+def main():
 
-ServerSocket.close()
+	initializeBlockchain()
+
+	ServerSocket = socket.socket()
+	host = '127.0.0.1'
+	port = 7000
+
+
+	try:	
+		ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		ServerSocket.bind((host, port))
+	except socket.error as e:
+		print(str(e))
+
+	print('Waiting for a Connection..')
+	ServerSocket.listen(5)
+
+	i = 1
+	while i <= 3:
+		connection, client_address = ServerSocket.accept()
+		print('Connected to: ' + client_address[0] + ':' + str(client_address[1]))
+		new_client= Server_Thread(connection, client_address[0] , client_address[1])
+		new_client.start()
+		client_list[client_address[1]%7000] = new_client
+		i+=1
+	
+
+	while True:
+		print("=========================================")
+		print("| Type 'BLOCK' to print the Blockchain  |")
+		print("| Type 'Q' to close the connection      |")
+		print("=========================================")
+		user_input = raw_input()
+		if user_input == "BLOCK":
+			print_blockchain()
+		elif user_input == "Q" or user_input == "q":
+			ServerSocket.close()
+			i = 1
+			while i <= 3:
+				client_list[i].connection.close()
+				i+=1
+			break
+
+
+if __name__ == "__main__":
+    main()
